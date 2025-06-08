649c1370902d2b2638d58e21bfad24d147eeef35
RenanCatini <renan.amaral@sou.unifal-mg.edu.br>
2025-06-08 17:29:32 -0300

    final

--- testbench.v
+++ testbench.v
@@ -15,10 +15,12 @@ module testbench;
     
     initial begin
         #1 resetn = 1'b0;  // Dá tempo para o clear acontecer
+
         #8 iin = 16'b101_000_0000011100;
         #8 iin = 16'b101_001_0000001010;
         #8 iin = 16'b001_000_001_0000000;
         #8 iin = 16'b100_000_0000000000;
+        
         #8 $finish;
         
     end
